125b3685d66dcb14ce7c3bc2604670cbf15e64c1
mellina20240 <naitmohamedmellina@gmail.com>
2025-09-28 19:51:39 +0100

    function

---
+++ after class project/calculator/index.js
@@ -0,0 +1,14 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+
+}
+
+function calculate(){
+}
